2ce6df7c6e36fcdf940456c4931d173bcaeb3ede
WasathTheekshana <wasath.vt@gmail.com>
2023-01-29 20:44:30 +0530

    dev: create components

---
+++ src/components/AddPost/AddPost.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AddPost.css'
+
+function AddPost() {
+  return (
+    <div>
+        <div className="addPost">
+            <div className="addPostContainer">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddPost
